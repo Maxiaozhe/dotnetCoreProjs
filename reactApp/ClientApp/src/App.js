3dc9b8e4c2e606b2f941b74ad625bf83029acfd9
馬 小哲 <xiaozhe_ma@jp.ricoh.com>
2019-06-07 13:24:00 +0900

    and typescript mindmap

--- reactApp/ClientApp/src/App.js
+++ reactApp/ClientApp/src/App.js
@@ -4,12 +4,12 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-import {GridView} from './components/GridView';
+import { GridView } from './components/GridView';
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
---
+++ reactApp/ClientApp/src/components/DataTable.ts
@@ -0,0 +1,4 @@
+interface DataTable{
+    head :
+    rows:[]
+}
